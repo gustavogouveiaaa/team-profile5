f9d4774eaf8f1a1a6f63489b6993f4abe7b20537
Gustavo Gouveia <111981511+gustavogouveiaaa@users.noreply.github.com>
2023-04-24 18:28:31 -0300

    Add files via upload

---
+++ TeamProfile/html/js/home.js
@@ -0,0 +1,51 @@
+let options = {
+    strings: ['Frontend Developer' ],
+    typeSpeed: 190,
+    backSpeed: 150,
+    backDelay: 150,
+    loop: true
+  };
+  
+  let typed = new Typed('.multiple-text',options);
+
+let options2 = {
+    strings: ['Frontend Developer' ],
+    typeSpeed: 190,
+    backSpeed: 150,
+    backDelay: 150,
+    loop: true
+  };
+  
+  let typed2 = new Typed('.multiple-text2',options2);
+
+let options3 = {
+    strings: ['estagiario' ],
+    typeSpeed: 190,
+    backSpeed: 150,
+    backDelay: 150,
+    loop: true
+  };
+  
+  let typed3 = new Typed('.multiple-text3',options3);
+
+let options4 = {
+    strings: ['Backend Developer' ],
+    typeSpeed: 190,
+    backSpeed: 150,
+    backDelay: 150,
+    loop: true
+  };
+  
+  let typed4 = new Typed('.multiple-text4',options4);
+
+let options5 = {
+  strings: ['Fullstack Developer ' ],
+    typeSpeed: 190,
+    backSpeed: 150,
+    backDelay: 150,
+    loop: true
+  };
+  
+  let typed5 = new Typed('.multiple-text5',options5);
+
+  
